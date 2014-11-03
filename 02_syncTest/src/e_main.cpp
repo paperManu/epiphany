f2fb384182148cfb320c4d665a7080c1aacafa1c
Emmanuel Durand <emmanueldurand@gmail.com>
2014-11-03 07:53:13 -0500

    Some updates to 02_syncTest, although some strange issues arise

--- 02_syncTest/src/e_main.cpp
+++ 02_syncTest/src/e_main.cpp
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <e_lib.h>
 
-char outbuf[128] SECTION("shared_dram");
+#include "common.h"
+
+//volatile Mailbox mailbox SECTION("shared_dram");
+volatile Mailbox* mailbox = (Mailbox*)e_emem_config.base;
 
 /*************/
 int main(void)
 {
-    e_coreid_t coreid;
-    coreid = e_get_coreid();
-    sprintf(outbuf, "Hi there, from 0x%03x!", coreid);
+    e_coreid_t coreid = e_get_coreid();
+    unsigned int row, col;
+    e_coords_from_coreid(coreid, &row, &col);
+
+    //volatile Mailbox* mailbox = (Mailbox*)e_emem_config.base;
+    mailbox->sync[row][col] = 1; //(int)(row * 10 + col);
+
     return 0;
 }
--- 02_syncTest/src/main.cpp
+++ 02_syncTest/src/main.cpp
@@ -1,9 +1,12 @@
+#include <cstddef>
 #include <iostream>
 #include <mutex>
 #include <thread>
 #include <vector>
 
 #include <e-hal.h>
+
+#include "common.h"
 
 using namespace std;
 
@@ -26,14 +29,42 @@ int main(int argc, char** argv)
         exit(1);
     }
 
-    if (e_load_group((char*)"e_main.srec", &epiphany, 0, 0, platform.rows, platform.cols, E_TRUE) == E_ERR)
+    if (e_load_group((char*)"e_main.srec", &epiphany, 0, 0, platform.rows, platform.cols, E_FALSE) == E_ERR)
     {
         cout << "Error while loading program into Epiphany" << endl;
         exit(1);
     }
 
-    unsigned int shmSize = 0x00400000;
-    e_alloc(&shm, 0x00000000, shmSize);
+    Mailbox mailbox;
+
+    e_alloc(&shm, 0x00000000, sizeof(mailbox));
+    for (int i = 0; i < 4; ++i)
+        for (int j = 0; j < 4; ++j)
+            mailbox.sync[i][j] = 0;
+
+    e_write(&shm, 0, 0, 0x0, &mailbox, sizeof(Mailbox));
+
+    cout << "Launching..." << endl;
+    e_start_group(&epiphany);
+
+    bool allSynced = false;
+    while (!allSynced)
+    {
+        e_read(&shm, 0, 0, offsetof(Mailbox, sync[0][0]), &(mailbox.sync), 16 * sizeof(int));
+        allSynced = true;
+        int count = 0;
+        cout << "syncing..." << endl;
+        for (unsigned int i = 0; i < 4; ++i)
+            for (unsigned int j = 0; j < 4; ++j)
+            {
+                cout << " --> " << i << " x " << j << " - " << mailbox.sync[i][j] << endl;
+                if (mailbox.sync[i][j] == 0)
+                    allSynced = false;
+                else
+                    count++;
+            }
+        cout << " - count = " << count << endl;
+    }
 
     e_free(&shm);
     e_finalize();
